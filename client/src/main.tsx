759899df8604862be94959cfc268347d93cd2bd5
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-11-10 00:50:32 +0100

    Elimina node_modules del repositorio

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './assets/components/Navbar/Navbar'
-import Footer from './assets/components/Footer/Footer'
-import Button from './assets/components/Button/Button'
-import CarouselComponent from './assets/components/carousel/Carousel'
-import AlertComponent from './assets/components/Alert/alert';
+import Header from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import Button from './components/Button/Button'
+import CarouselComponent from './components/carousel/Carousel'
+import AlertComponent from './components/Alert/alert';
 
 
 
