297082642456144ee17b5f3d105ced604a00f756
Jorge <jorestuard@gmail.com>
2023-11-13 19:33:42 +0100

    create model phone

--- server/src/config/dbConfig.ts
+++ server/src/config/dbConfig.ts
@@ -1,10 +1,12 @@
 import DATA from './dbData';
 import DbConfig from '../types/dbConfig';
 
-export const DBCONFIG: DbConfig = {
+const DBCONFIG: DbConfig = {
     host:'localhost',
     database: DATA.database,
     user: 'root',
     password: DATA.password,
     port: 3306
 }
+
+export default DBCONFIG
