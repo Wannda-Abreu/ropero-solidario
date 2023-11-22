3816dfcb8bba980f37aeb2ea532d17f0d6c8f22a
Victor-N <victornashillustration@gmail.com>
2023-11-22 15:01:41 +0100

    feature(server/db)updated squema

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts?|js?)$',
   modulePathIgnorePatterns: ['<rootDir>/node_modules','<rootDir>/tests/vitest',],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/index.ts'],
   coverageDirectory: 'coverage-jest',
   coverageReporters: ['text', 'html'],
   watchPathIgnorePatterns: ['node_modules',],
