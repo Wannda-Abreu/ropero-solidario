d94957c42908d4486272d09d6a0515aabe369416
Victor-N <victornashillustration@gmail.com>
2023-11-30 06:56:19 +0100

    feature(root)Manually Merged Front and backend Testing

---
+++ client/tests/footer.test.tsx
@@ -0,0 +1,42 @@
+import { BrowserRouter } from 'react-router-dom';
+import { render} from '@testing-library/react';
+import logotype from "../../assets/Logos/white-logo.png";
+
+describe("Testing that the Footer is Rendered", () => {
+    function Footer() {
+        return (
+          <main>
+            <section aria-label="Footer" className="banner" data-testid = "Footer">
+              <div className="footer-content" data-testid = "footer-content">
+                <div className="footer-text" data-testid = "footer-text">
+                  <h6>Ropero Solidario</h6>
+                  <h6>Fundación Instituto San José</h6>
+                  <h6>Hermanos de San Juan de Dios</h6>
+                </div> 
+                <img
+                  aria-label="logotype"
+                  className="footer-logo"
+                  src={logotype}
+                  alt="Image of logotype"
+                  data-testid = "footer-logo"
+                />
+                <div className="footer-text column" data-testid = "footer-text column">
+                  <div aria-label="copyright" className="copyright-text" data-testid = "copyright-text">
+                    © 2023 Copyright:
+                  </div>
+                </div>
+              </div>
+            </section>
+          </main>
+        );
+    }
+
+    beforeEach(() => {
+        render(
+            <BrowserRouter>
+                <Footer />
+            </BrowserRouter>
+        );
+      });  
+
+})
